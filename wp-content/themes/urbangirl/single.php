7653704ad20b7c29119f0aa4d04b25d2801ff36d
Krimo <krimo.hafsaoui@misterassur.com>
2013-10-28 09:18:09 +0100

    fixing bugs

--- wp-content/themes/urbangirl/single.php
+++ wp-content/themes/urbangirl/single.php
@@ -7,10 +7,10 @@
             <?php // (has_post_thumbnail() && the_post_thumbnail() !== NULL) ? the_post_thumbnail() : displayBackupImage(); ?>
             <?php
                 if (the_post_thumbnail() !== NULL) {
-                    var_dump('IT WORK.')
+                    var_dump('IT WORK.');
                     the_post_thumbnail();
                 } else {
-                    var_dump('IT FAIL.')
+                    var_dump('IT FAIL.');
                     displayBackupImage();
                 }
             ?>
