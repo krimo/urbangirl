0b220d3d99c7ba1c36907c22763ee3cbd55010ee
Krimo <krimo.hafsaoui@misterassur.com>
2013-10-29 17:08:14 +0100

    redirect old permalinks to new ones in articles

--- wp-content/themes/urbangirl/single.php
+++ wp-content/themes/urbangirl/single.php
@@ -2,7 +2,33 @@
 <!-- Main article -->
 <article class="row" role="main">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <?php ug_set_post_views(get_the_ID()); ?>
+
+    <?php
+        $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+        $uriArray = explode('/', $_SERVER[REQUEST_URI]);
+        $urlSlug = ($uriArray[0] === '~urbangi') ? $uriArray[1] : $uriArray[0];
+        $sitesArray = array(
+            'actualites' => 'http://urbangirl-actualites.fr',
+            'mode' => 'http://urbangirl-mode.fr',
+            'beaute' => 'http://urbangirl-beaute.fr',
+            'mariage' => 'http://urbangirl-mariage.fr',
+            'maman' => 'http://urbangirl-maman.fr',
+            'couple' => 'http://urbangirl-couple.fr',
+            'gastronomie' => 'http://urbangirl-gastronomie.fr',
+            'deco' => 'http://urbangirl-decoration.fr',
+            'bonnes-adresses' => 'http://urbangirl-sorties.fr',
+            'non-classe' => 'http://96.30.54.222/~urbangi/non-classe',
+        );
+
+        if (array_key_exists($urlSlug, $sitesArray)) {
+            $new_link = str_replace('http://96.30.54.222/~urbangi/'.$urlSlug, $sitesArray[$urlSlug], $actual_link);
+            header("HTTP/1.1 301 Moved Permanently");
+            header("Location: $new_link");
+        }
+
+        ug_set_post_views(get_the_ID());
+    ?>
     <aside class="large-4 columns hide-for-small">
         <figure>
             <?php
