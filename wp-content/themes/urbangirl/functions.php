27cba1f1ce96de4b9b17e375f34348e1b80eef44
Krimo <krimo.hafsaoui@misterassur.com>
2013-10-30 15:11:46 +0100

    changed email message for sharing article

--- wp-content/themes/urbangirl/functions.php
+++ wp-content/themes/urbangirl/functions.php
@@ -517,7 +517,7 @@ function ug_send_article() {
 
         $subject = $_POST['ug-share-name'].' vous recommande un article sur UrbanGirl';
 
-        $message = 'On vous recommande l\'article suivant : '.$_POST['ug-share-permalink'];
+        $message = 'Une amie (le mail de la personne) vous recommande de lire l\'article suivant : '.$_POST['ug-share-permalink'].PHP_EOL.'Nous vous souhaitons une agréable lecture !'
 
         wp_mail(
             $cleanEmails,
