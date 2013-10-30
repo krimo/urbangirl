6e8a3e8b327a35317419fdd404cb2b648c0c6830
Krimo <krimo.hafsaoui@misterassur.com>
2013-10-30 15:57:06 +0100

    added pagination sub-categories

--- wp-content/themes/urbangirl/sub-category.php
+++ wp-content/themes/urbangirl/sub-category.php
@@ -17,8 +17,16 @@
         <hr>
         <?php endwhile; ?>
         <!-- post navigation -->
-        <?php next_posts_link(); ?>
-        <?php previous_posts_link(); ?>
+        <?php
+        $big = 999999999; // need an unlikely integer
+
+        echo paginate_links( array(
+            'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+            'format' => '?paged=%#%',
+            'current' => max( 1, get_query_var('paged') ),
+            'total' => $wp_query->max_num_pages
+        ) );
+        ?>
         <?php else: ?>
         <!-- no posts found -->
         <?php endif; ?>
