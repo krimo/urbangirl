9183d2c2a760fe9f30552b5fa8270484e0ccbabc
Krimo <krimo.hafsaoui@misterassur.com>
2013-10-30 16:42:53 +0100

    added pagination sub-categories

--- wp-content/themes/urbangirl/sub-category.php
+++ wp-content/themes/urbangirl/sub-category.php
@@ -5,11 +5,10 @@
 
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <!-- post -->
-
         <article class="clearfix ug-subcategory-article">
         	<div class="crop left">
 	            <a href="<?php the_permalink(); ?>" class="picture">
-	                <?php (has_post_thumbnail()) ? the_post_thumbnail() : displayBackupImage(); ?>
+	                <?php (has_post_thumbnail() && the_post_thumbnail() !== NULL) ? the_post_thumbnail() : displayBackupImage(); ?>
 	            </a>
             </div>
             <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <br><small>publié il y a <em><?= human_time_diff( get_the_time('U'), current_time('timestamp') ); ?></em></small></h3>
