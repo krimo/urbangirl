d9b55a3ad376fd12ce64a0ebcea06257452fb23e
Krimo <krimo.hafsaoui@misterassur.com>
2013-10-30 13:28:20 +0100

    moved ug-panel to its own template

---
+++ wp-content/themes/urbangirl/ug-article-panel.php
@@ -0,0 +1,19 @@
+<article class="ug-panel">
+    <ul class="ug-tag-list">
+        <?php
+            $post_categories = wp_get_post_categories( $post->ID );
+            foreach ($post_categories as $c) {
+                echo '<li><a href="'.get_category_link( get_cat_ID(get_category( $c )->name) ).'">'.get_category( $c )->name.'</a></li>';
+            }
+        ?>
+    </ul>
+
+    <div class="crop">
+        <?php (has_post_thumbnail() && the_post_thumbnail() !== NULL) ? the_post_thumbnail() : displayBackupImage(); ?>
+    </div>
+
+    <footer class="ug-panel-inner">
+        <p><em><?= 'il y a '.human_time_diff( get_the_time('U'), current_time('timestamp') ); ?></em></p>
+        <h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
+    </footer>
+</article>
