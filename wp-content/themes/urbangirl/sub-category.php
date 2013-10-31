577922a29f1870fdfafef22d98ecdbe81fd3939b
Krimo <krimo.hafsaoui@misterassur.com>
2013-10-31 11:07:46 +0100

    of course last minute changes

--- wp-content/themes/urbangirl/sub-category.php
+++ wp-content/themes/urbangirl/sub-category.php
@@ -4,9 +4,7 @@
         <?= (category_description()) ? category_description() : '<p class="lead">Bienvenue sur la categorie <em>'.single_cat_title('',false).'</em></p>'; ?>
 
         <?php
-            var_dump(get_query_var('cat'));
-            $catId = 6;
-            query_posts('posts_per_page=15&cat='.$catId);
+            query_posts('posts_per_page=15&cat='.get_query_var('cat'));
             if ( have_posts() ) : while ( have_posts() ) : the_post();
         ?>
         <!-- post -->
