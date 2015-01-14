c60495993936c935b7eb7ea12f61d09ba9e636af
krimo <krimo.hafsaoui@gmail.com>
2015-01-14 08:47:38 -0600

    cleaning up

--- wp-content/themes/urbangirl/header.php
+++ wp-content/themes/urbangirl/header.php
@@ -141,21 +141,21 @@
     <meta name="description" content="<?php bloginfo('description'); ?>">
     <title><?= (is_home()) ? get_bloginfo('name') : ucfirst($pagename) . ' | ' . get_bloginfo('name'); ?></title>
 
-    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-152x152.png">
-    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon-196x196.png" sizes="196x196">
-    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon-160x160.png" sizes="160x160">
-    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon-96x96.png" sizes="96x96">
-    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon-16x16.png" sizes="16x16">
-    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon-32x32.png" sizes="32x32">
+    <link rel="apple-touch-icon" sizes="57x57" href="<?= get_template_directory_uri(); ?>/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?= get_template_directory_uri(); ?>/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?= get_template_directory_uri(); ?>/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?= get_template_directory_uri(); ?>/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?= get_template_directory_uri(); ?>/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?= get_template_directory_uri(); ?>/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?= get_template_directory_uri(); ?>/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?= get_template_directory_uri(); ?>/apple-touch-icon-152x152.png">
+    <link rel="icon" type="image/png" href="<?= get_template_directory_uri(); ?>/favicon-196x196.png" sizes="196x196">
+    <link rel="icon" type="image/png" href="<?= get_template_directory_uri(); ?>/favicon-160x160.png" sizes="160x160">
+    <link rel="icon" type="image/png" href="<?= get_template_directory_uri(); ?>/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="<?= get_template_directory_uri(); ?>/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="<?= get_template_directory_uri(); ?>/favicon-32x32.png" sizes="32x32">
     <meta name="msapplication-TileColor" content="#000000">
-    <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/mstile-144x144.png">
+    <meta name="msapplication-TileImage" content="<?= get_template_directory_uri(); ?>/mstile-144x144.png">
 
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     <script src="<?= get_template_directory_uri().'/js/' ?>modernizr.min.js"></script>
